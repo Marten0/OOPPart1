f09acd86a8fdb1ecd6a624f9b01b23f749c06b10
Opilane TTHK <opilane@tthk.ee>
2021-11-25 09:56:07 +0200

    OOPPart1

---
+++ OOPPart1/OOPCat/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace OOPCat
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double hunger;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hunger = 0;
+                Console.WriteLine($"The cat {name} with {color} color has been created");
+            }
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Color
+            {
+                get { return Color; }
+            }
+
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+            public void Sleep()
+            {
+                Console.WriteLine("Sleeps");
+                hunger += 0.2; //hunger = hunger + 0.2
+            }
+
+            public void Meow()
+            {
+                Console.WriteLine("Meow-meow!");
+            }
+
+           
+        }
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat("Kitty", "Orange");
+
+            while (myCat.Hunger != 1)
+            {
+                myCat.Sleep();
+            }
+
+            myCat.Meow();
+
+            Console.WriteLine($"Name: {myCat.Name}");
+            Console.WriteLine($"Level of hunger: {myCat.Hunger}");
+        }
+    }
+}
---
+++ OOPPart1/OOPPart1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace OOPPart1
+{
+    class Program
+    {
+        class Dog
+        {
+             string name;
+             int spots;
+             double happiness;
+
+            public Dog(string _name, int _spots)//constructor
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} with {spots} spots has been created.");
+            }
+
+            //getters for class properties
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public int Spots
+            {
+                get { return spots; }
+            }
+
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+
+            public void Barks()
+            {
+                Console.WriteLine("Woof-woof!");
+                happiness += 0.2; //happiness = happiness + 0.2
+            }
+
+            public void WagTail()
+            {
+                Console.WriteLine("Wiggle-wiggle");
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Koer", 15);
+            //myDog.happiness = 10;
+            //myDog.name = "Kõuts";
+
+            while (myDog.Happiness != 1)
+            {
+                myDog.Barks();
+            }
+
+            myDog.WagTail();
+
+            Console.WriteLine("Rename your dog:");
+            string userInput = Console.ReadLine();
+            myDog.Rename(userInput);
+
+            Console.WriteLine($"Name: {myDog.Name}");
+            Console.WriteLine($"Level of happiness: {myDog.Happiness}");
+        }
+    }
+}
